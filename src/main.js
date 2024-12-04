e498341cd8f4cc3d38f1b741507250568163f5bc
malgorzata <malgorzata.kodehode@gmail.com>
2024-12-04 09:42:35 +0100

    third commit

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import "./style.css";
 import javascriptLogo from "./javascript.svg";
 import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter.js";
-
+import { defineConfig } from "./vite.config.js";
 document.querySelector("#app").innerHTML = `
   <div>
     <h1>"Data Visualization!</h1>
